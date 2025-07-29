a300a20884fd42dd66590d38f86e3c31353e337a
00parkyh <00parkyh@naver.com>
2025-07-29 14:39:12 +0900

    ãwelcomejava

--- src/jungol/반복제어문3/Main.java
+++ src/jungol/반복제어문3/Main.java
@@ -1,20 +1,21 @@
 package jungol.반복제어문3;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
+        int count=1;
+        int Asc = 97; //a 아스키 코드값 97
 
-        for (int i = 1; i<num; i++) {
-            for (int j = 1; j<=i; j++) {
-                System.out.println();
-            }
-            for (int q = 0; ;q++) {
-                System.out.print(q);
-            }
+        for (int i=1; i<=4; i++) {
 
+            for (int x = 1; x<=i;x++ ) {
+                System.out.print((char) Asc + " ");
+                Asc++;
+                for (int w = 1; w<=x; w++) {
+                    System.out.print(count + " ");
+                    count++;
+                }
+            }
+            System.out.println();
         }
 
     }
