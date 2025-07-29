2c0e7af3bd2f6c0d852bd447f582fdae464a3ca5
00parkyh <00parkyh@naver.com>
2025-07-29 15:05:55 +0900

    welcomejava

--- src/day05/Welcomejava.java
+++ src/day05/Welcomejava.java
@@ -6,33 +6,34 @@ public class Welcomejava {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int callNumber ;
-        String name;
+
+        String userMobile ;
+        String userName;
         int manueNumber ;
         boolean state = true;
 
         System.out.print("당신의 이름을 입력하세요 : ");
-        name = sc.nextLine();
+        userName = sc.nextLine();
         System.out.print("연락처를 입력하세요 : ");
-        callNumber = sc.nextInt();
+        userMobile = sc.nextLine();
 
         while (state) {
-            System.out.println("**************************************************\n");
+            System.out.println("**************************************************");
             System.out.println("          Welcome to Shopping Mall"          );
             System.out.println("          Welcome to Book Market!"           );
-            System.out.println("**************************************************\n");
+            System.out.println("**************************************************");
             System.out.println("1. 고객 정보 확인하기      4. 바구니에 항목 추가하기");
             System.out.println("2. 장바구니 상품 목록 보기  5. 장바구니의 항목 수량 줄이기");
             System.out.println("3. 장바구니 비우기         6. 장바구니의 항목 삭제하기");
             System.out.println("7. 영수증 표시하기         8.종료");
-            System.out.println("**************************************************\n");
+            System.out.println("**************************************************");
             System.out.print("메뉴 번호를 선택해주세요 ");
             manueNumber = sc.nextInt();
 
             switch (manueNumber) {
                 case 1:
                     System.out.println("현재 고객 정보 : ");
-                    System.out.printf("이름 %s   연락처 %d",name,callNumber);
+                    System.out.printf("이름 %s   연락처 %s \n",userName,userMobile);
                     break;
                 case 2: System.out.println("장바구니 상품 목록 보기");break;
                 case 3: System.out.println("장바구니 비우기");break;
